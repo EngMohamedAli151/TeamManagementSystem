b50bca0eeb9f297f7bc4f23b4115bda429e745e2
mohamed_ali <moh15amed1ali@gmail.com>
2024-05-20 22:20:45 +0300

    Add project files.

---
+++ TeamDataBase/Model/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamDataBase.Model
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string NickName { get; set; }=string.Empty;
+        public string Mail { get; set; }
+        [PasswordPropertyText]
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        
+
+    }
+}
